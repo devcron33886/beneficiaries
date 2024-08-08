286313950d1029ef406e8dfd5aabc4174a7bd90c
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-08-08 23:43:43 +0200

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             ZamukaSeeder::class,
             EcdSeeder::class,
             MvtcSeeder::class,
+            MicrocreditSeeder::class,
 
         ]);
     }
